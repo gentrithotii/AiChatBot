9bc54b602adb50ae5e907c278141f45d0f7b6130
Gentrithotii <Gentrithoti@hotmail.com>
2025-09-18 19:47:23 +0200

    Further improvement and simplification to the level and refactored the function now its smaller and cleaner

--- src/main/java/com/gentrit/aichatbot/dto/ExplanationLevel.java
+++ src/main/java/com/gentrit/aichatbot/dto/ExplanationLevel.java
@@ -1,7 +1,28 @@
 package com.gentrit.aichatbot.dto;
 
+import lombok.Getter;
+import org.springframework.ai.openai.OpenAiChatOptions;
+
 public enum ExplanationLevel {
-    BEGINNER ,
-    INTERMEDIATE,
-    ADVANCED
+    BEGINNER("Beginner - Use simple explanations and basic terminology.", "gpt-4.1", 2000, 0.3),
+    INTERMEDIATE("Intermediate - Focus on practical use cases and best practices.", "gpt-4.1-mini", 1250, 0.4),
+    ADVANCED("Advanced - Explain deeper concepts, trade-offs, and optimizations", "gpt-4.1-mini", 750, 0.4);
+
+    @Getter
+    private final String description;
+    private final String model;
+    private final int maxTokens;
+    private final double temperature;
+
+
+    ExplanationLevel(String description, String model, int maxTokens, double temperature) {
+        this.description = description;
+        this.model = model;
+        this.maxTokens = maxTokens;
+        this.temperature = temperature;
+    }
+
+    public OpenAiChatOptions openAiChatOptions() {
+        return OpenAiChatOptions.builder().model(model).maxCompletionTokens(maxTokens).temperature(temperature).build();
+    }
 }
--- src/main/java/com/gentrit/aichatbot/service/impl/AiChatServiceImpl.java
+++ src/main/java/com/gentrit/aichatbot/service/impl/AiChatServiceImpl.java
@@ -27,22 +27,12 @@ public class AiChatServiceImpl implements AiChatService {
                 Intermediate – Focus on practical use cases and best practices.
                 Advanced – Explain deeper concepts, trade-offs, and optimizations.
          */
-        String chatLvl = "";
-        if (level.equals(ExplanationLevel.BEGINNER)) {
-            chatLvl = "Beginner - Use simple explanations and basic terminology.";
-        } else if (level.equals(ExplanationLevel.INTERMEDIATE)) {
-            chatLvl = "Intermediate - Focus on practical use cases and best practices.";
-        } else if (level.equals(ExplanationLevel.ADVANCED)) {
-            chatLvl = "Advanced - Explain deeper concepts, trade-offs, and optimizations";
-        }
+
         String systemTemplate = String.format("""
                 You are a math teacher named Mathbot with teaching level of: 
                 %s
                 Be clear, concise, factual, and brief in your responses. If additional information is required, ask the user instead of guessing. Only handle topics related to mathematics. 
-                If asked something off-topic, politely inform the user that you are solely a math teacher. Respect any instructions provided by the user, if applicable.""", chatLvl);
-
-
-        OpenAiChatOptions chatOptions = OpenAiChatOptions.builder().temperature(0.5).maxCompletionTokens(1000).model("gpt-4.1-mini").build();
+                If asked something off-topic, politely inform the user that you are solely a math teacher. Respect any instructions provided by the user, if applicable.""", level.getDescription());
 
 
         SystemMessage sm = SystemMessage.builder().text(systemTemplate).build();
@@ -53,10 +43,8 @@ public class AiChatServiceImpl implements AiChatService {
 
         Prompt prompt = Prompt.builder()
                 .messages(sm, um)
-                .chatOptions(chatOptions)
+                .chatOptions(level.openAiChatOptions())
                 .build();
-
-//        ChatResponse chatResponse = chatModel.call(prompt);
 
         return chatModel.stream(prompt).mapNotNull(generation -> generation.getResult().getOutput().getText());
     }
